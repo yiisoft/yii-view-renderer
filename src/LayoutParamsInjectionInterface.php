270369f22bcdf027814899d2eba8c70994eb549a
Sergei Predvoditelev <sergey.predvoditelev@gmail.com>
2020-08-19 20:46:17 +0300

    init (#1)

---
+++ src/LayoutParamsInjectionInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Yii\View;
+
+interface LayoutParamsInjectionInterface
+{
+
+    /**
+     * Returns parameters for added to layout.
+     *
+     * For example:
+     *
+     * ```
+     * [
+     *     'paramA' => 'something',
+     *     'paramB' => 42,
+     *     ...
+     * ]
+     * ```
+     *
+     * @return array
+     */
+    public function getLayoutParams(): array;
+}
---
+++ src/LinkTagsInjectionInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Yii\View;
+
+interface LinkTagsInjectionInterface
+{
+
+    /**
+     * Returns array of link tags for register via {@see \Yiisoft\View\WebView::registerLinkTag()}.
+     * Optionally, you may set the key that identifies the link tag via `__key`.
+     *
+     * For example:
+     *
+     * ```php
+     * [
+     *     [
+     *         '__key' => 'favicon',
+     *         'rel' => 'icon',
+     *         'type' => 'image/png',
+     *         'href' => '/myicon.png',
+     *     ],
+     *     ...
+     * ]
+     * ```
+     *
+     * @return array
+     */
+    public function getLinkTags(): array;
+}

58467cfb13d7c4e5c419777794b436dd24498304
thenotsoft <thenotsoft@gmail.com>
2022-07-22 15:32:34 +0200

    cleanup

--- tests/views/localize/es/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-echo 'View localize content es';
--- tests/views/localize/layout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-/**
- * @var string $content
- */
-
-echo 'Layout localize ' . $content;

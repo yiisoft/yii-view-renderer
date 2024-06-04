f8fa35fd090867c21693c09ad5c4e6c286a3d3be
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-06-04 22:15:15 +0300

    Fix events configuration for Yii Debug

--- config/events-web.php
+++ config/events-web.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 use Yiisoft\View\Event\WebView\AfterRender;
 use Yiisoft\Yii\View\Debug\WebViewCollector;
 
+if (!($params['yiisoft/yii-debug']['enabled'] ?? false)) {
+    return [];
+}
+
 return [
     AfterRender::class => [
         [WebViewCollector::class, 'collect'],

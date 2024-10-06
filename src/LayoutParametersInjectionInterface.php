fec315e75901835717fdd12b59a300d48bbf81e8
TiaNex <service@tianex.com>
2024-10-06 12:34:27 +0800

    Update LayoutParametersInjectionInterface.php
    get csrftoken once

--- src/LayoutParametersInjectionInterface.php
+++ src/LayoutParametersInjectionInterface.php
@@ -4,25 +4,52 @@ declare(strict_types=1);
 
 namespace Yiisoft\Yii\View\Renderer;
 
+use LogicException;
+use Yiisoft\Csrf\CsrfTokenInterface;
+use Yiisoft\Csrf\CsrfTrait;
+
 /**
- * LayoutParametersInjectionInterface is an interface that must be implemented by classes to inject layout parameters.
+ * `CsrfViewInjection` injects the necessary data into the view to protect against a CSRF attack.
  */
-interface LayoutParametersInjectionInterface
+final class CsrfViewInjection implements CsrfParametersInjectionInterface
 {
+    use CsrfTrait;
+
+    public const DEFAULT_META_ATTRIBUTE_NAME = 'csrf';
+    public const DEFAULT_PARAMETER_NAME = 'csrf';
+    public const META_TAG_KEY = 'csrf';
+
+    private string $metaAttributeName = self::DEFAULT_META_ATTRIBUTE_NAME;
+    private string $parameterName = self::DEFAULT_PARAMETER_NAME;
+
+    public function __construct(private CsrfTokenInterface $token)
+    {
+    }
+
+
     /**
-     * Returns parameters for added to layout.
-     *
-     * For example:
-     *
-     * ```
-     * [
-     *     'paramA' => 'something',
-     *     'paramB' => 42,
-     *     ...
-     * ]
-     * ```
-     *
-     * @psalm-return array<string, mixed>
+     * @throws LogicException when CSRF token is not defined
      */
-    public function getLayoutParameters(): array;
+    public function getCsrfParameters(): array
+    {
+        $tokenValue = $this->token->getValue();
+        $csrf = new Csrf(
+            $tokenValue,
+            $this->getFormParameterName(),
+            $this->getHeaderName(),
+        );
+        return [
+            [
+                $this->parameterName => $csrf
+            ],
+            [
+                self::META_TAG_KEY => [
+                    'name' => $this->metaAttributeName,
+                    'content' => $tokenValue
+                ],
+            ],
+        ];
+    }
+
+
 }
